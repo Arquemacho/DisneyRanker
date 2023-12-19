bfbcf65de4ddc2db4c2e3766bfbd52be446ebee3
Arquemacho <camachoa.nicolas@javeriana.edu.co>
2023-12-19 15:52:05 -0500

    Now Using local DB

--- src/MoviePoster.js
+++ src/MoviePoster.js
@@ -1,35 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import './TournamentStyles.css';
+import postersData from './movie_posters.json'; // Import your JSON file
 
 const MoviePoster = ({ title }) => {
     const [posterUrl, setPosterUrl] = useState('');
 
     useEffect(() => {
-        const apiKey = '83ec3237b21273a67ebe413154877f59'; // Replace with your actual API key
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(title)}`;
-
-        axios.get(url)
-            .then(response => {
-                const results = response.data.results;
-                if (results.length > 0) {
-                    const movie = results[0];
-                    const posterPath = movie.poster_path;
-                    setPosterUrl(`https://image.tmdb.org/t/p/w500${posterPath}`);
-                }
-            })
-            .catch(error => {
-                console.error('Error fetching data: ', error);
-            });
+        // Retrieve poster URL from the imported JSON data
+        const posterPath = postersData[title];
+        if (posterPath) {
+            setPosterUrl(posterPath);
+        } else {
+            console.error('Poster not found for: ', title);
+        }
     }, [title]);
 
     return (
         <div>
             <h3>{title}</h3>
-            {posterUrl ? <img src={posterUrl} alt={`Poster of ${title}`} className="movie-poster" /> : <p>Loading...</p>}
+            {posterUrl ? <img src={posterUrl} alt={`Poster of ${title}`} className="movie-poster" /> : <p>Poster not found</p>}
         </div>
     );
 };
-
 
 export default MoviePoster;
