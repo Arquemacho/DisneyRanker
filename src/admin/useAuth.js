38f89755b48765075b498fd7dd26f18ee4059c01
Arquemacho <camachoa.nicolas@javeriana.edu.co>
2023-12-25 12:43:10 -0500

    Update useAuth.js

--- src/admin/useAuth.js
+++ src/admin/useAuth.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const ADMIN_PASSWORD = 'Nicocam2001.';
@@ -8,19 +8,20 @@ const useAuth = () => {
     const [error, setError] = useState('');
     const navigate = useNavigate();
 
-    const validatePassword = (inputPassword) => {
-        console.log("Attempting to validate password");
-        if (inputPassword === ADMIN_PASSWORD) {
-            console.log("Password correct, navigating to admin");
-            setIsAuthenticated(true);
+    useEffect(() => {
+        if (isAuthenticated) {
             navigate('/admin');
+        }
+    }, [isAuthenticated, navigate]);
+
+    const validatePassword = (inputPassword) => {
+        if (inputPassword === ADMIN_PASSWORD) {
+            setIsAuthenticated(true);
             setError('');
         } else {
-            console.log("Password incorrect");
             setError('Incorrect password. Please try again.');
         }
     };
-
 
     return { isAuthenticated, validatePassword, error };
 };
