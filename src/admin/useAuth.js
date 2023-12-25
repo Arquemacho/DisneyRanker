25c3ea5f227d554dc0b2ac7547fab4966b3404dc
Arquemacho <camachoa.nicolas@javeriana.edu.co>
2023-12-24 19:12:03 -0500

    Update useAuth.js

--- src/admin/useAuth.js
+++ src/admin/useAuth.js
@@ -9,14 +9,18 @@ const useAuth = () => {
     const navigate = useNavigate();
 
     const validatePassword = (inputPassword) => {
+        console.log("Attempting to validate password");
         if (inputPassword === ADMIN_PASSWORD) {
+            console.log("Password correct, navigating to admin");
             setIsAuthenticated(true);
             navigate('/admin');
             setError('');
         } else {
+            console.log("Password incorrect");
             setError('Incorrect password. Please try again.');
         }
     };
+
 
     return { isAuthenticated, validatePassword, error };
 };
